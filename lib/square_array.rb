e47b632acd4272ac9fdd7836bd5efa624b1dca11
James Benjamin <james.abenj@gmail.com>
2019-09-07 01:34:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   counter = 0 
   while array[counter] do 
-    puts array.new(array[counter]**2)
+    array[counter]**2
     counter += 1 
   end
+  puts array.new(array)
 end
